d4b389525d8bd36703e5ffb28ff84eac0779bd84
iiVSX!! <36075767+iiVSX@users.noreply.github.com>
2018-12-10 06:31:15 +0900

    Update generator.js

--- Random_Name_Generator/generator.js
+++ Random_Name_Generator/generator.js
@@ -1,3 +1,11 @@
 var trainedData = require("./trained-data.js");
 
 trainedData.firstNames = uncompressEmptyPart(trainedData.firstNames);
+
+/**
+ * 랜덤한 한국 이름을 생성한다.
+ * 
+ * @param {boolean} isMale 
+ */
+function generate(trainedDataMatrix) {
+}
