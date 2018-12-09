412c2828898d2e3d47c47c06bf5bd1de97a73e2b
iiVSX!! <36075767+iiVSX@users.noreply.github.com>
2018-12-10 06:33:35 +0900

    Update generator.js

--- Random_Name_Generator/generator.js
+++ Random_Name_Generator/generator.js
@@ -12,8 +12,10 @@ function generate(trainedDataMatrix) {
 
         // 요소들의 가중치에 비례한 확률로 랜덤 뽑기
     let pick = (count, item) => {
-            let sum = 0;
-            let selected = 0;
-    
+        let sum = 0;
+        let selected = 0;
+        for (let i = 0; i < count; i++) {
+                sum += item(i);
+        }
     }
 }
